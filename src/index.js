4a8e347363c28393a1e2147e135be563b4886383
Narit Charoenval <naritsociety@gmail.com>
2020-11-10 15:31:05 +0700

    Add databases connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
+
+const db = require('./db');
+const DB_HOST = process.env.DB_HOST;
+db.connect(DB_HOST);
 
 const port = process.env.PORT || 5000;
 
